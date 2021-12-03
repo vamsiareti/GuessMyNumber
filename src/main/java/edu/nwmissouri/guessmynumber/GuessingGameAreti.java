55b22b293e9750ee670bba1cb9d112551d30e606
vamsiareti <vamsiareti2020@gmail.com>
2021-12-03 11:42:57 -0600

    End of the class

--- src/main/java/edu/nwmissouri/guessmynumber/GuessingGameAreti.java
+++ src/main/java/edu/nwmissouri/guessmynumber/GuessingGameAreti.java
@@ -18,40 +18,41 @@ public class GuessingGameAreti {
 
     public static void main(String[] args) {
         // TODO: change computerNumber to secretNumber Hint: Right-click / Refactor / Rename
-        int computerNumber = (int) (Math.random() * 100 + 1);
+        int laptopId = (int) (Math.random() * 100 + 1);
         int userAnswer = 0;
-        System.out.println("The correct guess would be " + computerNumber);
+        System.out.println("The correct guess would be " + laptopId);
         int count = 1;
 
-        while (userAnswer != computerNumber) {
+        while (userAnswer != laptopId) {
             // TODO: create 3 local variables for the arguments so we know what they mean
             // showInputDialog - the first argument is always null (no parent)
-            // message = "Enter a guess between 1 and 100" // what type?
-            // title = "Guessing Game" // what type?
-            // messageType = JOptionPane.INFORMATION_MESSAGE
+             String message = "Enter a guess between 1 and 100";
+             String title = "Guessing Game";
+             int messageType = JOptionPane.INFORMATION_MESSAGE;
             // Works the same as original 3 but more expressive - this is an example of a what? Hint: enum
             String response = JOptionPane.showInputDialog(null,
-                    "Enter a guess between 1 and 100", "Guessing Game", 3);
+                    message, "Guessing Game", 3);
             userAnswer = Integer.parseInt(response);
 
-            JOptionPane.showMessageDialog(null, "" + determineGuess(userAnswer, computerNumber, count));
+            JOptionPane.showMessageDialog(null, "" + displayMessage(userAnswer, laptopId, count));
             count++;
         }
     }
 
     // TODO: Refactor / Rename determineGuess to calculateMessage
-    public static String determineGuess(int userAnswer, int computerNumber, int count) {
+    public static String displayMessage(int userAnswer, int computerNumber, int count) {
         // TODO: Instead of "Try Number" which sounds like we should try that as a guess...
         // Say "You've used " + count + guesses.
         // If count is one, use "guess", else use "guesses" HINT: Ternary operator
+        String s = (count == 1) ? "guesses." : "guesses.";
         if (userAnswer <= 0 || userAnswer > 100) {
             return "Your guess is invalid";
         } else if (userAnswer == computerNumber) {
             return "Correct!\nTotal Guesses: " + count;
         } else if (userAnswer > computerNumber) {
-            return "Your guess is too high, try again.\nTry Number: " + count;
+            return "Your guess is too high, try again.\nAttempts: " + count + s;
         } else if (userAnswer < computerNumber) {
-            return "Your guess is too low, try again.\nTry Number: " + count;
+            return "Your guess is too low, try again.\nAttempts: " + count + s;
         } else {
             return "Your guess is incorrect\nTry Number: " + count;
         }
